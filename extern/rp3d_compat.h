a5eeb191d0472d5828f2b1976704a99a8c40e4ab
Chris Goodin <cgoodin@cavs.msstate.edu>
2026-02-11 09:29:59 -0600

    one more update to extern/rp3d_compat.h

--- extern/rp3d_compat.h
+++ extern/rp3d_compat.h
@@ -1,2 +1,24 @@
 #pragma once
-#include "reactphysics3d/decimal.h"
+#include <cstdint>
+
+// Only define if not already defined in this translation unit
+#ifndef RP3D_UINT_TYPES_DEFINED
+#define RP3D_UINT_TYPES_DEFINED
+
+#ifndef uint32
+using uint32 = uint32_t;
+#endif
+
+#ifndef uint64
+using uint64 = uint64_t;
+#endif
+
+#ifndef int32
+using int32 = int32_t;
+#endif
+
+#ifndef int64
+using int64 = int64_t;
+#endif
+
+#endif  // RP3D_UINT_TYPES_DEFINED
